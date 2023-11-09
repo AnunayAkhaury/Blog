3d10c85c0ad1725e463934faa2861d050c08a23b
AnunayAkhaury <anunay2003@gmail.com>
2023-11-09 01:57:09 -0800

    more template logic

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all_posts.html")
 
 def post_detail(request):
     pass
